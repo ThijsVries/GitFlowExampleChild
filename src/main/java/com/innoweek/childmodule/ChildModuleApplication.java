3066d41ce7b2fb815c6be74671862756486454be
Vries <vries01@LOCGOV.NL>
2020-09-17 10:34:25 +0200

    Made a small change to the child-module

--- src/main/java/com/innoweek/childmodule/ChildModuleApplication.java
+++ src/main/java/com/innoweek/childmodule/ChildModuleApplication.java
@@ -1,13 +1,9 @@
 package com.innoweek.childmodule;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class ChildModuleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChildModuleApplication.class, args);
+		System.out.print("I have made a small change to the child-module");
 	}
 
 }
