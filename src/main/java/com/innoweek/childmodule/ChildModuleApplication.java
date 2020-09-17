7f56944165a55f51ab944ed92dac498f02aa52ad
Vries <vries01@LOCGOV.NL>
2020-09-17 10:54:44 +0200

    Finished new feature!

--- src/main/java/com/innoweek/childmodule/ChildModuleApplication.java
+++ src/main/java/com/innoweek/childmodule/ChildModuleApplication.java
@@ -5,6 +5,7 @@ public class ChildModuleApplication {
 	public static void main(String[] args) {
 		System.out.print("I have made a small change to the child-module");
 		System.out.print("Geweldige nieuwe feature!");
+		System.out.print("Klaar.");
 	}
 
 }
