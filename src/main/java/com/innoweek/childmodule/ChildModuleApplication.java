bc2e03518d520154064c11a930134b09c865f754
Vries <vries01@LOCGOV.NL>
2020-09-17 10:09:00 +0200

    Init repo

---
+++ src/main/java/com/innoweek/childmodule/ChildModuleApplication.java
@@ -0,0 +1,13 @@
+package com.innoweek.childmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innoweek/childmodule/ChildModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innoweek.childmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
