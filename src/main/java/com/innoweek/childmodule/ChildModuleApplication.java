dc480165e46e9682f4459eb1d94982ad6a292958
Vries <vries01@LOCGOV.NL>
2020-09-17 16:45:16 +0200

    Added new code

--- src/main/java/com/innoweek/childmodule/ChildModuleApplication.java
+++ src/main/java/com/innoweek/childmodule/ChildModuleApplication.java
@@ -1,12 +1,14 @@
 package com.innoweek.childmodule;
 
+import com.innoweek.component.TestComponent;
+
 public class ChildModuleApplication {
 
-	public static void main(String[] args) {
-		System.out.print("I have made a small change to the child-module");
-		System.out.print("Geweldige nieuwe feature!");
-		System.out.print("Klaar.");
-		System.out.print("Wat werk aan een nieuwe mooie feature.");
-	}
-
+    public static void main(String[] args) {
+        System.out.print("I have made a small change to the child-module");
+        System.out.print("Geweldige nieuwe feature!");
+        System.out.print("Klaar.");
+        System.out.print("Wat werk aan een nieuwe mooie feature.ffff");
+        System.out.print(TestComponent.versionString);
+    }
 }
