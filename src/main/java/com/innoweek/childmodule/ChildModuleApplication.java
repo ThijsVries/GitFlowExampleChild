5d5b96edf45bf97eb749eda8094fb9dbe31a51f7
Vries <vries01@LOCGOV.NL>
2020-09-17 10:54:11 +0200

    Added an amazing new feature WIP

--- src/main/java/com/innoweek/childmodule/ChildModuleApplication.java
+++ src/main/java/com/innoweek/childmodule/ChildModuleApplication.java
@@ -4,6 +4,7 @@ public class ChildModuleApplication {
 
 	public static void main(String[] args) {
 		System.out.print("I have made a small change to the child-module");
+		System.out.print("Geweldige nieuwe feature!");
 	}
 
 }
